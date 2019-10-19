f38d148f1168aab638bab1937c31ee5c570ff2cd
KingdomCross <alexchheng1998@yahoo.com>
2019-10-19 13:19:24 -0700

    Fixed main and cylinder

--- cylinderTypeImp.cpp
+++ cylinderTypeImp.cpp
@@ -3,8 +3,8 @@
 //
 
 #include <iostream>
-#include <iomanip>
 #include "cylinderType.h"
+
 using namespace std;
 
 void cylinderType::print() const
@@ -16,4 +16,34 @@ void cylinderType::print() const
     cout << "Radius: " << radius << endl;
     cout << "Circumference: " << getCircumference() << endl;
     cout << "Area: " << getArea() << endl;
+}
+
+void cylinderType::setHeight(double h) {
+    height = h;
+}
+
+void cylinderType::setBaseCenter(double x, double y) {
+
+}
+
+void cylinderType::setCenterRadiusHeight(double x, double y, double r, double h) {
+
+}
+
+double cylinderType::getHeight() const {
+    return height;
+}
+
+double cylinderType::getVolume() const {
+    return 3.1416 * radius * radius * height;
+}
+
+double cylinderType::getSurfaceArea() const {
+    return (2 * 3.1416 * radius * radius) + (2 * 3.1416 * radius * height);
+}
+
+cylinderType::cylinderType(double x, double y, double r, double h)
+        :circleType(x,y,r)
+{
+    height = h;
 }
